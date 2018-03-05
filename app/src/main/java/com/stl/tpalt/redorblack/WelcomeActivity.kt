78d6fc85247157aa9a696d7d1db98f8ec1b89a93
RudyKr <8DsjGb3Zct>
2018-03-05 01:38:31 +0100

    :sparkles: Welcome page !

---
+++ app/src/main/java/com/stl/tpalt/redorblack/WelcomeActivity.kt
@@ -0,0 +1,40 @@
+package com.stl.tpalt.redorblack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+import android.preference.PreferenceManager
+
+class WelcomeActivity : AppCompatActivity() {
+
+    private val firstLaunchPref = "firstLaunchPref"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+//        val buttonRules= findViewById<Button>(R.id.button_regles)
+        val buttonStart= findViewById<Button>(R.id.button_start)
+        val prefs= PreferenceManager.getDefaultSharedPreferences(this)
+
+        val isFirstLaunch = prefs.getBoolean(firstLaunchPref, true)
+        if (!isFirstLaunch) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        buttonStart.setOnClickListener(
+                {
+                    _ ->
+                    val editor = prefs.edit()
+                    editor.putBoolean(firstLaunchPref, false)
+                    editor.apply()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                })
+    }
+}
+
