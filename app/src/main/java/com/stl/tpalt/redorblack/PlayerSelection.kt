bc6bdeed954dedd6a0045b4b226a3edc6151968d
ThibCharpy <thibault.charpignon@wanadoo.fr>
2018-03-12 16:31:38 +0100

    C4ESTMOIQUIFAITTOUT :bug:

--- app/src/main/java/com/stl/tpalt/redorblack/PlayerSelection.kt
+++ app/src/main/java/com/stl/tpalt/redorblack/PlayerSelection.kt
@@ -3,7 +3,6 @@ package com.stl.tpalt.redorblack
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.constraint.ConstraintLayout
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.*
@@ -11,24 +10,19 @@ import com.shawnlin.numberpicker.NumberPicker
 import org.jetbrains.anko.toast
 import kotlinx.android.synthetic.main.activity_player_selection.*
 
+//15 cartes dans la pyramide
+//x*52 cartes
+//5 cartes par joueur
+//(x*52 - 15)/5 = nb de joueurs max
+//x=(nbJ*5 + 15)/52
+
 class PlayerSelection : AppCompatActivity() {
-    lateinit var layoutNbDecks : ConstraintLayout
     lateinit var numberPickerNbDecks : NumberPicker
-    lateinit var textViewNbDecks : TextView
-    lateinit var layoutPlayerList : ConstraintLayout
-    lateinit var listviewPlayerList : ListView
-    lateinit var layoutNewPlayer : ConstraintLayout
     lateinit var buttonAddNewPlayer : Button
     lateinit var editTextPlayerName : EditText
-    lateinit var layoutStartGame : ConstraintLayout
     lateinit var buttonStartGame : Button
 
-    //15 cartes dans la pyramide
-    //x*52 cartes
-    //5 cartes par joueur
-    //(x*52 - 15)/5 = nb de joueurs max
-    //x=(nbJ*5 + 15)/52
-
+    lateinit var playerListView: ListView
 
 
     lateinit var playerList: PlayerList
@@ -38,22 +32,23 @@ class PlayerSelection : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player_selection)
 
-        layoutNbDecks       = layout_nbdecks
-        numberPickerNbDecks = number_picker
-        textViewNbDecks     = tv_nbdecks
+        numberPickerNbDecks = findViewById(R.id.number_picker)
 
-        layoutPlayerList    = layout_playerlist
-        listviewPlayerList  = listview_playerlist
+        buttonAddNewPlayer  = findViewById(R.id.button_add)
+        editTextPlayerName  = findViewById(R.id.input_playername)
 
-        layoutNewPlayer     = layout_newplayer
-        buttonAddNewPlayer  = button_add
-        editTextPlayerName  = input_playername
+        buttonStartGame     = findViewById(R.id.button_startgame)
 
-        layoutStartGame     = layout_startgame
-        buttonStartGame     = button_startgame
+        playerListView = findViewById(R.id.listview_playerlist)
+        val player_list = mutableListOf(
+                Player("Rudy"),
+                Player("Yannick"),
+                Player("Thibault"))
+        playerList = PlayerList()
 
+        val adapter = PlayerListAdapter(this, player_list)
+        playerListView.adapter = adapter
 
-//        val adapter : ArrayAdapter<Player> = ArrayAdapter(this, android.R.layout.simple_list_item_1, playerList)
         
         //button binding
         buttonStartGame.setOnClickListener { _ ->
@@ -63,18 +58,21 @@ class PlayerSelection : AppCompatActivity() {
             startActivity(intent)
             onPause()
         }
+
         buttonAddNewPlayer.setOnClickListener { _ ->
             val newplayername : String = editTextPlayerName.text.toString()
-            if (newplayername == "")
-                return@setOnClickListener
+            /*if (newplayername == "")
+                return@setOnClickListener*/
             editTextPlayerName.setText("")
-            playerList.pl_list.add(Player(newplayername))
+            //playerList.pl_list.add(Player(newplayername))
+            player_list.add(Player(newplayername))
+            adapter.notifyDataSetChanged()
             updateNumberPicker()
-//            toast("newplayername added.")
             toast("$newplayername added.\n$playerList")
         }
 
-        //edittext
+        // Enable the button when the user put text into the editText
+        // Else the Button to Add is disable
         editTextPlayerName.addTextChangedListener(object: TextWatcher {
             override fun afterTextChanged(p0: Editable?) {
             }
@@ -98,7 +96,4 @@ class PlayerSelection : AppCompatActivity() {
         numberPickerNbDecks.minValue=playerList.pl_list.size/8+1
     }
 
-    override fun finish() {
-        super.finish()
-    }
 }
