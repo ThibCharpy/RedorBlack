7967e03b5aeb2168d58d4af045453d54744396bc
ThibCharpy <thibault.charpignon@wanadoo.fr>
2018-03-20 15:32:49 +0100

    petite modif language :beer:

--- app/src/main/java/com/stl/tpalt/redorblack/activities/SettingsActivity.kt
+++ app/src/main/java/com/stl/tpalt/redorblack/activities/SettingsActivity.kt
@@ -3,7 +3,9 @@ package com.stl.tpalt.redorblack.activities
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.support.annotation.RequiresApi
 import android.support.v7.app.AppCompatActivity
+import android.widget.ImageButton
 import com.stl.tpalt.redorblack.R
 import kotlinx.android.synthetic.main.activity_settings.*
 import java.util.*
@@ -19,29 +21,34 @@ class SettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_settings)
 
         if(Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN_MR1) {
-
-            val flagFr = flagFr
-            flagFr.setOnClickListener {
-                val locale = Locale("fr")
-                val dm = resources.displayMetrics
-                val config = resources.configuration
-                config.setLocale(locale)
-                resources.updateConfiguration(config, dm)
-                super.onConfigurationChanged(config)
+            val flagFr= findViewById<ImageButton>(R.id.flagFr)
+            flagFr.setOnClickListener{
+                _ ->
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                    setLanguage("fr")
+                }
                 val intent = Intent(this, WelcomeActivity::class.java)
                 startActivity(intent)
             }
-            val flagEn = flagEn
-            flagEn.setOnClickListener {
-                val locale = Locale("en")
-                val dm = resources.displayMetrics
-                val config = resources.configuration
-                config.setLocale(locale)
-                resources.updateConfiguration(config, dm)
-                super.onConfigurationChanged(config)
+
+            val flagEn= findViewById<ImageButton>(R.id.flagEn)
+            flagEn.setOnClickListener{
+                _ ->
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+                    setLanguage("en")
+                }
                 val intent = Intent(this, WelcomeActivity::class.java)
                 startActivity(intent)
             }
         }
+    }
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    fun setLanguage(language: String) {
+        val locale = Locale(language)
+        val dm = resources.displayMetrics
+        val config = resources.configuration
+        config.setLocale(locale)
+        resources.updateConfiguration(config,dm)
+        super.onConfigurationChanged(config)
     }
 }
--- app/src/main/java/com/stl/tpalt/redorblack/activities/WelcomeActivity.kt
+++ app/src/main/java/com/stl/tpalt/redorblack/activities/WelcomeActivity.kt
@@ -1,11 +1,15 @@
 package com.stl.tpalt.redorblack.activities
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.support.annotation.RequiresApi
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import android.widget.ImageButton
 import com.stl.tpalt.redorblack.R
+import java.util.*
 
 //can go to RulesActivity or PlayerSelectionActivity from here
 class WelcomeActivity : AppCompatActivity() {
@@ -35,7 +39,6 @@ class WelcomeActivity : AppCompatActivity() {
                     val intent = Intent(this, RulesActivity::class.java)
                     startActivity(intent)
                 })
-
     }
 }
 
